75b9af0480b777ddef167d1c26edec154ba77c3d
Thiago Magalhães Furtado <thiago.furtado@lead.fortaleza>
2023-05-08 16:24:19 -0300

    Pokedex - Versão 2

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,7 @@
             v-for="pokemon in filtered_pokemons"
             :key="pokemon.name"
           >
-            <v-card @click="show_pokemon(get_id(pokemon))">
+            <v-card class="pokemon" @click="show_pokemon(get_id(pokemon))">
               <v-container>
                 <v-row class="mx-0 d-flex justify-content-center">
                   <img
@@ -29,7 +29,7 @@
                       pokemon
                     )}.png`"
                     :alt="pokemon.name"
-                    width="100%"
+                    width="200px"
                   />
                 </v-row>
                 <h2 class="text-center">{{ get_name(pokemon) }}</h2>
@@ -53,27 +53,50 @@
               />
             </v-col>
             <v-col cols="7">
-              <v-row>
-                <v-chip
-                  label
-                  v-for="type in selected_pokemon.types"
-                  :key="type.slot"
-                  class="mr-2"
-                >
-                  {{ type.type.name }}
-                </v-chip>
-                <v-chip label>
-                  Altura: {{ selected_pokemon.height * 2.54 }} cm
-                </v-chip>
-                <v-chip label class="ml-2">
-                  Peso: {{ (selected_pokemon.weight * 0.45359237).toFixed(0) }} Kg
-                </v-chip>
-              </v-row>
+              <v-container>
+                <v-row>
+                  <v-container>
+                    <h3>Tipos de pokemon</h3>
+                  </v-container>
+                  <v-container class="tipos">
+                    <v-chip
+                      label
+                      v-for="type in selected_pokemon.types"
+                      :key="type.slot"
+                      class="mr-2"
+                    >
+                      {{ type.type.name }}
+                    </v-chip>
+                  </v-container>
+                </v-row>
+              </v-container>
+              <v-container class="mt-2">
+                <v-row>
+                  <v-container>
+                    <h3>Características do pokemon</h3>
+                  </v-container>
+                  <v-container>
+                    <v-chip label class="características">
+                      Altura: {{ selected_pokemon.height / 10 }} cm
+                    </v-chip>
+                  </v-container>
+                  <v-container class="características">
+                    <v-chip label>
+                      Peso: {{ (selected_pokemon.weight / 10) }} Kg
+                    </v-chip>
+                  </v-container>
+                  <v-container>
+                    <v-chip label class="características">
+                      Experiência Base: {{ selected_pokemon.base_experience }} XP
+                    </v-chip>
+                  </v-container>
+                </v-row>
+              </v-container>
             </v-col>
           </v-row>
 
-          <h2 class="text-center">Movimentos</h2>
-          <h3 class="text-center" v-if="filter_moves(selected_pokemon).length==0">Esse pokemon não possue nenhum move ainda</h3>
+          <h2 class="text-center titulos">Movimentos</h2>
+          <h3 class="text-center" v-if="filter_moves(selected_pokemon).length==0">Esse pokemon não possue nenhum movimento ainda</h3>
           <v-simple-table class="text-center" v-if="filter_moves(selected_pokemon).length>0">
             <template>
               <thead>
@@ -94,45 +117,173 @@
             </template>
           </v-simple-table>
 
-          <h2 class="text-center">Variações</h2>
+          <h2 class="text-center titulos">Variações</h2>
           <v-row class="d-flex align-center">
             <v-col cols="4">
               <img
                 :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${selected_pokemon.id}.png`"
-                width="100%"
+                width="200px"
               />
             </v-col>
             <v-col cols="4">
               <img
                 :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/${selected_pokemon.id}.png`"
-                width="100%"
+                width="200px"
               />
             </v-col>
             <v-col cols="4">
               <img
                 :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${selected_pokemon.id}.png`"
-                width="100%"
+                width="200px"
               />
             </v-col>
             <v-col cols="4">
               <img
                 :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${selected_pokemon.id}.png`"
-                width="100%"
+                width="200px"
               />
             </v-col>
             <v-col cols="4">
               <img
                 :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/shiny/${selected_pokemon.id}.png`"
-                width="100%"
+                width="200px"
               />
             </v-col>
             <v-col cols="4">
               <img
                 :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/${selected_pokemon.id}.png`"
-                width="100%"
+                width="200px"
               />
             </v-col>
           </v-row>
+
+          <h2 class="text-center titulos">Versões</h2>
+          <v-row class="d-flex align-center">
+            <v-col cols="4">
+              <img
+                :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/red-blue/${selected_pokemon.id}.png`"
+                width="200px"
+              />
+            </v-col>
+            <v-col cols="4">
+              <img
+                :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/yellow/${selected_pokemon.id}.png`"
+                width="200px"
+              />
+            </v-col>
+            <v-col cols="4">
+              <img
+                :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-ii/crystal/${selected_pokemon.id}.png`"
+                width="200px"
+              />
+            </v-col>
+            <v-col cols="4">
+              <img
+                :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-ii/gold/${selected_pokemon.id}.png`"
+                width="200px"
+              />
+            </v-col>
+            <v-col cols="4">
+              <img
+                :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-ii/silver/${selected_pokemon.id}.png`"
+                width="200px"
+              />
+            </v-col>
+            <v-col cols="4">
+              <img
+                :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iii/emerald/${selected_pokemon.id}.png`"
+                width="200px"
+              />
+            </v-col>
+            <v-col cols="4">
+              <img
+                :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iii/firered-leafgreen/${selected_pokemon.id}.png`"
+                width="200px"
+              />
+            </v-col>
+            <v-col cols="4">
+              <img
+                :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iii/ruby-sapphire/${selected_pokemon.id}.png`"
+                width="200px"
+              />
+            </v-col>
+            <v-col cols="4">
+              <img
+                :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iv/diamond-pearl/${selected_pokemon.id}.png`"
+                width="200px"
+              />
+            </v-col>
+            <v-col cols="4">
+              <img
+                :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iv/heartgold-soulsilver/${selected_pokemon.id}.png`"
+                width="200px"
+              />
+            </v-col>
+            <v-col cols="4">
+              <img
+                :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iv/platinum/${selected_pokemon.id}.png`"
+                width="200px"
+              />
+            </v-col>
+            <v-col cols="4">
+              <img
+                :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/${selected_pokemon.id}.png`"
+                width="200px"
+              />
+            </v-col>
+            <v-col cols="4">
+              <img
+                :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-vi/omegaruby-alphasapphire/${selected_pokemon.id}.png`"
+                width="200px"
+              />
+            </v-col>
+            <v-col cols="4">
+              <img
+                :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-vi/x-y/${selected_pokemon.id}.png`"
+                width="200px"
+              />
+            </v-col>
+            <v-col cols="4">
+              <img
+                :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-vii/icons/${selected_pokemon.id}.png`"
+                width="200px"
+              />
+            </v-col>
+            <v-col cols="4">
+              <img
+                :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-vii/ultra-sun-ultra-moon/${selected_pokemon.id}.png`"
+                width="200px"
+              />
+            </v-col>
+            <v-col cols="4">
+              <img
+                :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-viii/icons/${selected_pokemon.id}.png`"
+                width="200px"
+              />
+            </v-col>
+          </v-row>
+
+          <h2 class="text-center titulos">Estatísticas</h2>
+          <v-container class="d-flex align-center">
+            <v-container>
+              <h4>{{ selected_pokemon.stats[0].stat.name }}: </h4>{{ selected_pokemon.stats[0].base_stat }}
+            </v-container>
+            <v-container>
+              <h4>{{ selected_pokemon.stats[1].stat.name }}: </h4>{{ selected_pokemon.stats[1].base_stat }}
+            </v-container>
+            <v-container>
+              <h4>{{ selected_pokemon.stats[2].stat.name }}: </h4>{{ selected_pokemon.stats[2].base_stat }}
+            </v-container>
+            <v-container>
+              <h4>{{ selected_pokemon.stats[3].stat.name }}: </h4>{{ selected_pokemon.stats[3].base_stat }}
+            </v-container>
+            <v-container>
+              <h4>{{ selected_pokemon.stats[4].stat.name }}: </h4>{{ selected_pokemon.stats[4].base_stat }}
+            </v-container>
+            <v-container>
+              <h4>{{ selected_pokemon.stats[5].stat.name }}: </h4>{{ selected_pokemon.stats[5].base_stat }}
+            </v-container>
+          </v-container>
         </v-container>
       </v-card>
     </v-dialog>
@@ -229,5 +380,18 @@ export default {
   background-size: cover !important;
   background-position: center;
   min-height: 100vh;
+}
+
+.pokemon {
+  min-height: 160px;
+  max-height: 354px;
+}
+
+.titulos {
+  margin-top: 46px;
+}
+
+.tipos, .características {
+  margin-top: -22px;
 }
 </style>
